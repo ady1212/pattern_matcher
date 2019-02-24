9ceb4ee33511f296413d7585b132f931b2a813e0
adam_szabo <adam_Szabo@epam.com>
2019-02-24 21:44:53 +0100

    Implementing a lexer with TokenTypes, Tokens and a Lexer. Whichgets an input as String(for now) and goes through it with a matcher. If the matcher find a TokenType, which is defined as an enum, it creates a new token and puts it in the result list

---
+++ pattern-matching-with-reflexive-parser/src/main/java/hu/elte/patternmatching/pattern_matcher_with_reflexive_parser/App.java
@@ -0,0 +1,13 @@
+package hu.elte.patternmatching.pattern_matcher_with_reflexive_parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
