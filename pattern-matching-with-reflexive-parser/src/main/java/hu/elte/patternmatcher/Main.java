f81c8bbd079660d0ff1ec54d7d02ade334877a7d
adam_szabo <adam_Szabo@epam.com>
2019-03-03 21:01:05 +0100

    made a complete revamp in the lexer, it can read now from files, and indentify various token types and write them on the console. If it can go successfully to the end of the file it writes a Success, else it says the unidentified token

---
+++ pattern-matching-with-reflexive-parser/src/main/java/hu/elte/patternmatcher/Main.java
@@ -0,0 +1,26 @@
+package hu.elte.patternmatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    private static Map<String,String> identifierValueMap;
+    
+    public static void main(String[] args) {
+        Lexer lexer = new Lexer("./resources/Input.txt");
+        identifierValueMap = new HashMap<>();
+        System.out.println("Lexical Analysis");
+        System.out.println("-----------------");
+        while (!lexer.isFinished()) {
+            System.out.printf("%-18s :  %s \n", lexer.currentLexema(), lexer.currentToken());
+            lexer.moveAhead();
+        }
+
+        if (lexer.isSuccessful()) {
+            System.out.println("Success!");
+        } else {
+            System.out.println(lexer.errorMessage());
+        }
+
+    }
+}
